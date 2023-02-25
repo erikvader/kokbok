57b742a58c6b45cf158f1e4a5241d14cb7f3968b
Erik Rimskog <erik.rimskog@gmail.com>
2023-02-25 20:43:17 +0100

    pretty print exception in generator

--- generator/src/kokbok/core.clj
+++ generator/src/kokbok/core.clj
@@ -1,10 +1,11 @@
 (ns kokbok.core
   (:gen-class)
   (:require
+   [io.aviso.exception :as aviso]
    [clojure.java.io :refer [file]]
    [kokbok.generation :refer [generate-recipes]]))
 
-(defn -main [& args]
+(defn inner-main [args]
   (when (not= 1 (count args))
     (throw (ex-info "invalid usage" {:args args})))
 
@@ -13,3 +14,10 @@
         books-path (file root "books.toml")
         ingredients-path (file root "ingredients.toml")]
     (generate-recipes recipes-path books-path ingredients-path)))
+
+(defn -main [& args]
+  (try (inner-main args)
+       (catch Exception e
+         (binding [*out* *err*]
+           (aviso/write-exception e))
+         (System/exit 1))))
